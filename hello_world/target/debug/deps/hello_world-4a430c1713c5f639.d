5c51dabe1073842e8e16af640b1b41495d86e38a
ulises <ulimarian2000@gmail.com>
2024-04-08 15:14:39 -0300

    [CREATE] first program

---
+++ hello_world/target/debug/deps/hello_world-4a430c1713c5f639.d
@@ -0,0 +1,5 @@
+H:\rust\hello_world\target\debug\deps\hello_world-4a430c1713c5f639.exe: src\main.rs
+
+H:\rust\hello_world\target\debug\deps\hello_world-4a430c1713c5f639.d: src\main.rs
+
+src\main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+H:\rust\hello_world\target\debug\hello_world.exe: H:\rust\hello_world\src\main.rs
